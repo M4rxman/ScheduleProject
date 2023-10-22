ae2ce936a33be648743da6c94a86b4e5a198e5fa
Olek <up202210478@fe.up.pt>
2023-10-22 17:01:23 +0100

    Fixed the FAKING TEST AAAAAAA

---
+++ src/DataCollectors/DataTest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include <fstream>
+#include <algorithm>
+#include "DataCollector.h"
+#include "../ClassesStruct/ClassesPerUc.h"
+
+using testing::Eq;
+
+//=============================================================================
+// Problem 1: ClassesPErUc
+//=============================================================================
+TEST(test_1, createDataCollector) {
+
+//EXPECT_EQ(7,n);
+
+}
