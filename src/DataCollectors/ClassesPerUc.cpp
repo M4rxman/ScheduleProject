9088d725f99cd6c5101421162a7fe0df12db11b9
Olek <up202210478@fe.up.pt>
2023-10-27 21:49:25 +0100

    ClassesPerUc gained working readFile, writeFile, addClassesToUc, all works, I'm happy

--- src/DataCollectors/ClassesPerUc.cpp
+++ src/DataCollectors/ClassesPerUc.cpp
@@ -3,28 +3,74 @@
 //
 
 #include "ClassesPerUc.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <map>
 
 // needs to be reworked may be
 
 void ClassesPerUc::readFile(ifstream &f) {
-    string ucCode,crutch_line, stringClassCode;
-    list<string> classCode;
-    getline(f, crutch_line); // an amazing crutch
-    while(!f.eof()){
-        getline(f, ucCode, ',');
+    string ucCode, stringClassCode;
+
+    if (!f.is_open()) {
+        cerr << "Error: File not open." << endl;
+        return;
+    }
+
+    // Temporary map to store class codes for each unique ucCode
+    map<string, list<string>> tempMap;
+
+    while (getline(f, ucCode, ',')) {
         getline(f, stringClassCode);
+        tempMap[ucCode].push_back(stringClassCode);
+    }
 
-        classCode.push_back(stringClassCode);
-        crutch_line = ucCode;
+    // Iterate through the map and use addClassesToUc function
+    for (const auto &entry : tempMap) {
+        const string &ucCode = entry.first;
+        const list<string> &classCodes = entry.second;
 
-        cPerUc.push_back(UcCode(ucCode, classCode));
-
-        while(crutch_line == ucCode){
-            getline(f, crutch_line, ',');
-            getline(f, stringClassCode);
-            classCode.push_back(stringClassCode);
+        for (const string &classCode : classCodes) {
+            addClassesToUc(classCode, ucCode);
         }
-        cPerUc.push_back(UcCode(ucCode, classCode));
+    }
+}
 
+void ClassesPerUc::addClassesToUc(string classCode, string ucCode) {
+    // Iterate through the list of UcCode objects
+    for (auto it = cPerUc.begin(); it != cPerUc.end(); ++it) {
+        // Check if the ucCode of the current UcCode object matches the provided ucCode
+        if (it->GetUcCode() == ucCode) {
+            // If it matches, add the classCode to the UcCode object
+            it->addClassCode(classCode);
+            return; // Exit the loop once class code is added
+        }
+    }
+
+    // If no matching UcCode object is found, create a new one
+    cPerUc.push_back(UcCode(ucCode, {classCode}));
+}
+
+void ClassesPerUc::writeFile() {
+    if(cPerUc.empty()){
+        std::cout << "ClassesPerUc list is empty" << std::endl;
+    }
+
+    for (UcCode uc : cPerUc) {
+        if(uc.ucCode == "UcCode"){
+            continue;
+        }
+
+        std::cout << "UcCode: " << uc.GetUcCode() << std::endl;
+
+        const std::list<std::string>& classCodes = uc.classCode;
+        std::cout << "Class Codes: ";
+        for (const std::string& code : classCodes) {
+            std::cout << code << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "-----------------------------------" << std::endl;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "ClassesPerUc.h"
 
 int main(int argc, char* argv[]) {
     std::cout << "AED" << std::endl;
 
+    ClassesPerUc myClass;
+    ifstream MyReadFile("C:\\Users\\alex2\\Downloads\\schedule\\Data\\classes_per_uc.csv");
+    myClass.readFile(MyReadFile);
+    myClass.writeFile();
 
 }
